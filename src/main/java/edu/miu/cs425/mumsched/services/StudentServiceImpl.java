530482856f0b2f50cee8f6a77a72a1e5c1328883
Demisew Mokonnen <demisewt@gmail.com>
2020-05-13 12:42:38 -0500

    student, faculty, course domains

---
+++ src/main/java/edu/miu/cs425/mumsched/services/StudentServiceImpl.java
@@ -0,0 +1,40 @@
+package edu.miu.cs425.mumsched.services;
+
+import edu.miu.cs425.mumsched.dao.StudentDao;
+import edu.miu.cs425.mumsched.domain.Student;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
+/**
+ * @author Demisew Mokonnen
+ * @2020
+ */
+public class StudentServiceImpl implements StudentService {
+    @Autowired
+    private StudentDao studentDao;
+    @Override
+    public Student findByEmail(String email) {
+        return studentDao.findByEmail(email);
+    }
+
+    @Override
+    public Student findById(Integer studentId) {
+        return studentDao.getOne(studentId);
+    }
+
+    @Override
+    public void delete(Integer studentId) {
+        studentDao.deleteById(studentId);
+    }
+
+    @Override
+    public void save(Student student) {
+        studentDao.save(student);
+    }
+
+    @Override
+    public List<Student> findAll() {
+        return studentDao.findAll();
+    }
+}
