af7a1fcba6e46e60d09feae5134920bdc84f2d46
Demisew Mokonnen <demisewt@gmail.com>
2020-05-13 11:41:25 -0500

    student, faculty, course domains

---
+++ src/main/java/edu/miu/cs425/mumsched/dao/FacultyDao.java
@@ -0,0 +1,17 @@
+package edu.miu.cs425.mumsched.dao;
+
+import edu.miu.cs425.mumsched.domain.Course;
+import edu.miu.cs425.mumsched.domain.Faculty;
+import edu.miu.cs425.mumsched.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface FacultyDao extends JpaRepository<Faculty,Integer> {
+    Faculty getFacultyByEmail(String email);
+    Faculty findByUser(User user);
+    List<Faculty> findAllByUserIs(User user);
+    List<Faculty> findAllByPreferredCoursesContains(Course course);
+}
