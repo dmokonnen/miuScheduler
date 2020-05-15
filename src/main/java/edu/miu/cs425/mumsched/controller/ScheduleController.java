5655d3fcff99c01fd874fafc08d4e4d92e95b0dd
Demisew Mokonnen <demisewt@gmail.com>
2020-05-14 20:01:50 -0500

    started on finalizing schedule

---
+++ src/main/java/edu/miu/cs425/mumsched/controller/ScheduleController.java
@@ -0,0 +1,13 @@
+package edu.miu.cs425.mumsched.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @author Demisew Mokonnen
+ * @2020
+ */
+@Controller
+@RequestMapping("/admin")
+public class ScheduleController {
+}
